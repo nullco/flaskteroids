12678385fd70b32b6b0925a5cdbee1702c3118a1
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-03-11 20:05:25 +0100

    feat: add more http methods to routes

--- flaskteroids/route.py
+++ flaskteroids/route.py
@@ -12,14 +12,26 @@ class Routes:
         self._app = app
 
     def root(self, *, to):
-        cname, caction = to.split('#')
-        self._register_view_func('/', cname, caction)
+        self._register_view_func('/', to, prefix='root')
+
+    def get(self, path, *, to):
+        self._register_view_func(path, to)
+
+    def post(self, path, *, to):
+        self._register_view_func(path, to, 'POST')
+
+    def put(self, path, *, to):
+        self._register_view_func(path, to, 'PUT')
+
+    def delete(self, path, *, to):
+        self._register_view_func(path, to, 'DELETE')
 
     def _get_controller_class(self, cname):
         controller_module = import_module(f'app.controllers.{cname}_controller')
         return getattr(controller_module, f'{cname.title()}Controller')
 
-    def _register_view_func(self, path, cname, caction):
+    def _register_view_func(self, path, to, method='GET', prefix=''):
+        cname, caction = to.split('#')
         ccls = self._get_controller_class(cname)
 
         def view_func(*args, **kwargs):
@@ -31,4 +43,9 @@ class Routes:
             view_template = render_template(f'{cname}/{caction}.html', **controller_instance.__dict__)
             return view_template
 
-        self._app.add_url_rule(path, view_func=view_func, methods=["GET"])
+        view_func_name = f"{cname}_{caction}"
+        if prefix:
+            view_func_name = f"{prefix}_{view_func_name}"
+        view_func.__name__ = view_func_name
+
+        self._app.add_url_rule(path, view_func=view_func, methods=[method])
