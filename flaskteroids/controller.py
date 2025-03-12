b52925a2058e56d3bc3cff4dca30b2f163d5babe
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-03-12 22:36:31 +0100

    chore: refactor before_action code a bit

--- flaskteroids/controller.py
+++ flaskteroids/controller.py
@@ -1,17 +1,21 @@
 def before_action(method_name, *, only=None):
     def setup_rule(controller):
-        before_action_method = getattr(controller, method_name)
+        before_action_ = getattr(controller, method_name)
         if not only:
             assert False, 'Not supported yet'
 
         for action_name in only:
             action = getattr(controller, action_name)
-
-            def new_action(action=action):
-                def wrapper(*args, **kwargs):
-                    before_action_method(*args, **kwargs)
-                    return action(*args, **kwargs)
-                return wrapper
-
-            setattr(controller, action_name, new_action())
+            setattr(controller, action_name, _chain(before_action_, action))
     return setup_rule
+
+
+def _chain(*actions):
+    def wrapper(*args, **kwargs):
+        res = None
+        for action in actions:
+            res = action(*args, **kwargs)
+            if res:
+                break
+        return res
+    return wrapper
