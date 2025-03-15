153773018f1de2c61f3ac046377a933ee8ffc19b
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-03-15 01:25:02 +0100

    wip: add some tests for cmd parsing

---
+++ tests/generators/migrations/test_cmd_parser.py
@@ -0,0 +1,45 @@
+import pytest
+from flaskteroids.generators.migrations import cmd_parser
+
+
+@pytest.mark.parametrize('cmd, expected', [
+    (
+        'CreateProducts',
+        {
+            'normalized_cmd': 'create_products',
+            'match': {
+                'action': 'create_table',
+                'captured_groups': ('products',)
+            }
+        }
+    ),
+    (
+        'AddPriceToProducts',
+        {
+            'normalized_cmd': 'add_price_to_products',
+            'match': {
+                'action': 'add_columns_to_table',
+                'captured_groups': ('price', 'products')
+            }
+        }
+    ),
+    (
+        'AddPriceAndTotalToProducts',
+        {
+            'normalized_cmd': 'add_price_and_total_to_products',
+            'match': {
+                'action': 'add_columns_to_table',
+                'captured_groups': ('price_and_total', 'products')
+            }
+        }
+    )
+])
+def test_command(cmd, expected):
+    res = cmd_parser.parse(cmd)
+    assert res['normalized_cmd'] == expected['normalized_cmd']
+    assert res['match'] == expected['match']
+
+
+def test_invalid_command():
+    with pytest.raises(Exception):
+        cmd_parser.parse('InvalidCommand', *[])
