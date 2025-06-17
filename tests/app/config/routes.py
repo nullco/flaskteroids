bb1cdf7154efcba579d94680d44167a251c4b46d
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-06-17 17:03:32 +0200

    fix: tests

--- tests/app/config/routes.py
+++ tests/app/config/routes.py
@@ -1,5 +1,3 @@
 def register(route):
     route.get('/up/', to="flaskteroids/health#show")
     route.get('/users/', to="users#index")
-    with route.resources('post'):
-        route.resources('comment')
