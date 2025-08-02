8611e1f106ef99e63204cc7e8184079ad356d471
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-08-02 20:11:23 +0200

    chore: remove duplicated field definition

--- flaskteroids/cli/generators/resource/cmd_parser.py
+++ flaskteroids/cli/generators/resource/cmd_parser.py
@@ -1,16 +1,19 @@
 import re
 from flaskteroids.cli.generators import cmd_parser
 from flaskteroids.inflector import inflector
+from flaskteroids.fields import fields
 
-_field_types = ['int', 'str', 'text', 'float', 'bool', 'json', 'references', 'belongs_to']
-_field_types_pattern = fr'{"|".join(k for k in _field_types)}'
-_fields_pattern = re.compile(fr'^([a-z_]+):({_field_types_pattern})(!?)$')
+
+_column_types_pattern = fr'{"|".join(k for k in fields.keys())}'
+_column_pattern = re.compile(fr'^([a-z_]+):({_column_types_pattern})(!?)$')
+_reference_pattern = re.compile(r'^([a-z_]+):(references|belongs_to)$')
 
 
 class _ResourceCommand:
     pattern = re.compile(r'([a-z_]+)')
     args = {
-        'fields': _fields_pattern
+        'fields': _column_pattern,
+        'reference': _reference_pattern
     }
 
     @classmethod
@@ -28,6 +31,10 @@ class _ResourceCommand:
             'fields': [
                 {'name': m.group(1), 'type': m.group(2)}
                 for m in args_matches['fields']
+            ],
+            'references': [
+                {'name': m.group(1), 'type': m.group(2)}
+                for m in args_matches['reference']
             ]
         }
 
