4409723d8dfdd02cc471e259bc3ab1f24d8d7c63
Juan Sebastian Quintero <5529078+nullco@users.noreply.github.com>
2025-11-06 21:53:43 +0100

    chore: improve mailer generator tests

--- tests/cli/generators/mailer/test_mailer_generator.py
+++ tests/cli/generators/mailer/test_mailer_generator.py
@@ -1,12 +1,5 @@
 import pytest
-from flaskteroids.cli.generators.mailer.generator import generate, _mailer, _html_view, _txt_view
-
-
-@pytest.fixture
-def cmd_parser(mocker):
-    parser = mocker.patch('flaskteroids.cli.generators.mailer.generator.cmd_parser')
-    parser.parse.return_value = {'parsed': {'snake': 'test_mailer', 'camel': 'TestMailer'}}
-    return parser
+from flaskteroids.cli.generators.mailer.generator import generate
 
 
 @pytest.fixture
@@ -19,17 +12,14 @@ def artifacts_builder(mocker):
 
 class TestMailerGenerator:
 
-    @pytest.mark.usefixtures('cmd_parser')
     def test_generate_creates_mailer_file(self, artifacts_builder, mocker):
         generate('test_mailer', ['welcome'])
         artifacts_builder.file.assert_any_call('app/mailers/test_mailer_mailer.py', mocker.ANY)
 
-    @pytest.mark.usefixtures('cmd_parser')
     def test_generate_creates_views_directory(self, artifacts_builder):
         generate('test_mailer', ['welcome'])
         artifacts_builder.dir.assert_called_once_with('app/views/test_mailer_mailer/')
 
-    @pytest.mark.usefixtures('cmd_parser')
     def test_generate_creates_view_files_for_actions(self, artifacts_builder, mocker):
         generate('test_mailer', ['welcome', 'goodbye'])
         expected_file_calls = [
